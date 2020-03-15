8d0fc73246925f10a1eb82f9657f39a224ca6630
Chris <lilunlogic@163.com>
2020-03-15 23:06:49 +0800

    first crwaler commit

---
+++ demo/web_crawler/crawler1.py
@@ -0,0 +1,12 @@
+import requests
+
+res = requests.get("http://test.beautystudio.com.cn/dg_admin_h5/#/login")
+print(res.status_code)
+
+res = requests.get("http://test.beautystudio.com.cn/dg_admin_h5/#/login",
+                   data={"user": "superadmin", "pass": "20192020buyerbee"})
+print(res.encoding)
+print(res.headers)
+print(res.content)
+print(res.cookies)
+print(res.text)
